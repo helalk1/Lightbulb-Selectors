49818271caddc043cb56ff09d36c12661f79fd30
helal kohzad <helalkohzad@gmail.com>
2023-10-19 14:16:06 +0100

    Helal Commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Write your code here
+let subtitle = document.querySelector(".subtitle");
+let bulb1 = document.querySelector("#lightbulb1");
+let bulb2 = document.querySelector("#lightbulb2");
+let bulb3 = document.querySelector("#lightbulb3");
+
+let count = 0;
+
+let allbulbs = document.querySelectorAll(".lightbulb");
+
+for (let bulb of allbulbs) {
+    bulb.addEventListener("click", function () {
+        count++;
+        subtitle.innerHTML = `You've clicked the lights ${count} times `;
+        bulb.classList.toggle("active");
+    });
+}
